2311eb28763ffde4344f38a355b6c9ad991b5223
rogermanzo <76231967+rogermanzo@users.noreply.github.com>
2020-12-20 10:55:50 -0400

    change apply default blank input

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 function sendMail(params) {
+    
+    var elemento1 = document.getElementById("fromName").value
+    var elemento2 = document.getElementById("fromEmail").value
+    var elemento3 = document.getElementById("msg").value
+    
+
     var tempParams = {
         from_name:document.getElementById("fromName").value,
         from_email:document.getElementById("fromEmail").value,
         message:document.getElementById("msg").value,
     };
 
+    if (elemento1 == "" || elemento2 == "" || elemento3 == ""){
+        alert("you must complete all the data")
+        return false
+    } else {
     emailjs.send('gmail', 'template_vp213yi', tempParams)
     .then(function(res){
         console.log("sucess:", res.status);
         document.getElementById("fromName").value="";
         document.getElementById("fromEmail").value="";
         document.getElementById("msg").value="";
-        alert('your message has been sent successfully');
+        alert(elemento1+', your message has been sent successfully');
     })
-
+    
+    }
 
 }
